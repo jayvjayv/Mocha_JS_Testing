d7b5da5cfe7dea40c4a1e9f4f6d5d451e5e3a2c5
jvanvan29 <jayvanostrand@Jays-MacBook-Pro.local>
2023-10-05 00:08:15 -0400

    Added package.json and test cases with assertions for each case

--- helloMocha.js
+++ helloMocha.js
@@ -7,22 +7,42 @@
 // 3. Multiplication
 // 4. Division
 
+let assert = require('assert');
+
 describe('Mathematical Operations - Test Suite', function(){
 
     it('Addition of two numbers', function(){
+        let a = 10;
+        let b = 10;
 
+        let c = a + b;
+
+        assert.equal(c, 20)
     });
 
     it('Subtraction of two numbers', function(){
+        let a = 10;
+        let b = 10;
 
+        let c = a - b;
+
+        assert.equal(c,0)
     });
 
     it('Multiplication of two numbers', function(){
+        let a = 10;
+        let b = 10;
 
+        let c = a * b
+        assert.equal(c, 100)
     });
 
     it('Division of two numbers', function(){
+        let a = 10;
+        let b = 10;
 
+        let c = a / b
+        assert.equal(c, 1)
     });
 
 });
