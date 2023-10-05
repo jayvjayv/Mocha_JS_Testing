1d8a5024ed8d2295f1268511dd7a601f698e176a
jvanvan29 <jayvanostrand@Jays-MacBook-Pro.local>
2023-10-04 23:34:43 -0400

    Setting up template a to describe a test for 4 test cases for: addition, subtraction, multiplication & division

---
+++ helloMocha.js
@@ -0,0 +1,28 @@
+//Test Suite - Mathematical Operations
+
+//Test Cases
+
+// 1. Addition
+// 2. Subtraction
+// 3. Multiplication
+// 4. Division
+
+describe('Mathematical Operations - Test Suite', function(){
+
+    it('Addition of two numbers', function(){
+
+    });
+
+    it('Subtraction of two numbers', function(){
+
+    });
+
+    it('Multiplication of two numbers', function(){
+
+    });
+
+    it('Division of two numbers', function(){
+
+    });
+
+});
